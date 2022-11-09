302f65a9c3264d892b68f243418ca06e897f527e
Dmytro Zhuchenko <mixakievchanen@gmail.com>
2022-11-10 01:35:31 +0200

    checkout start

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -5,6 +5,7 @@ import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.comopnent";
 import CartItem from "../cart-item/cart-item.component";
+import { Link } from "react-router-dom";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
@@ -15,7 +16,9 @@ const CartDropdown = () => {
           <CartItem cartItem={item} key={item.id} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Link to="checkout">
+        <Button>GO TO CHECKOUT</Button>
+      </Link>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
